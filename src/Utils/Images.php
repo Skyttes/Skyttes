59d7427eca193fc0f1190616bb7e80981a7f0705
Mia Vališová (Liliana) <118116415+lilianalillyy@users.noreply.github.com>
2023-09-26 14:52:03 +0200

    feat: add $outPath to Images methods, cleanup

--- src/Utils/Images.php
+++ src/Utils/Images.php
@@ -7,60 +7,72 @@ use Imagick;
 use RuntimeException;
 
 class Images {
-    public static function resizeImage(string $path, int $maxWidth = 1300, ?int $maxHeight = null, bool $exact = false): void
+    public static function resizeImage(string $path, int $maxWidth = 1300, ?int $maxHeight = null, bool $exact = false, ?string $outPath = null): string
     {
+        $outPath = $outPath ?? $path;
+
         $image = Image::fromFile($path);
         $image->resize(
-            min($image->getWidth(), $maxWidth), 
-            !$maxHeight ? null : min($image->getHeight(), $maxHeight), 
+            min($image->getWidth(), $maxWidth),
+            !$maxHeight ? null : min($image->getHeight(), $maxHeight),
             $exact ? Image::EXACT : Image::FIT
         );
-        $image->save($path);
+
+        $image->save($outPath);
+
+        return $outPath;
     }
 
-    public static function stripImage(string $path, int $compressionQuality = 70): void
+    public static function stripImage(string $path, int $compressionQuality = 70, ?string $outPath = null): string
     {
         if (!class_exists(Imagick::class)) {
-            $im = new Imagick();
-            $im->readImage($path);
-            $im->setImageCompressionQuality($compressionQuality);
-            $im->stripImage();
-            $im->writeImage($path);
-            $im->destroy();
-        } else {
             trigger_error("Imagick is required for " . self::class . "::stripImage()", E_USER_WARNING);
         }
+
+        $outPath = $outPath ?? $path;
+
+        $im = new Imagick();
+        $im->readImage($path);
+        $im->setImageCompressionQuality($compressionQuality);
+        $im->stripImage();
+        $im->writeImage($outPath);
+        $im->destroy();
+
+        return $outPath;
     }
 
-    public static function convertToWebp(string $path): string
+    public static function convertToWebp(string $path, int $quality = 100, ?string $outPath = null): string
     {
-        $ext = pathinfo($path)["extension"];
-
-        if ($ext === 'svg') {
-            trigger_error(".svg files are ignored and webp conversion is not supported.");
-            return $path;
-        }
-
         if (!function_exists('imagewebp')) {
             trigger_error("imagewebp function is required to convert images to webp", E_USER_WARNING);
         }
 
+        $pathInfo = pathinfo($path);
+
+        if (!is_array($pathInfo)) {
+            throw new RuntimeException("Malformed path provided.");
+        }
+
+        if ($pathInfo["extension"] === 'svg') {
+            trigger_error(".svg files are ignored and webp conversion is not supported.");
+            return $path;
+        }
+
         $image = Image::fromFile($path)->getImageResource();
 
-        $result = imagewebp($image, $path, 100);
-        if ($result === false) {
+        if (!$outPath) {
+            $outPath = str_replace($pathInfo["extension"], "webp", $path);
+        }
+
+        $result = imagewebp($image, $outPath, $quality);
+        if (!$result) {
             throw new RuntimeException('Failed to convert the provided image to webp format');
         }
 
         // Destroy image to free memory
         imagedestroy($image);
 
-        $newPath = str_replace($ext, '', $path) . 'webp';
-
-        rename($path, $newPath);
-        $path = $newPath;
-
-        return $path;
+        return $outPath;
     }
 
 }
